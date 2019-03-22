632300b6a50b3c935977eb42a49b723229927652
Nathanael Raway <nathanael.raway@gmail.com>
2019-03-22 09:42:33 -0500

    Removed fa dependencies and will just add svg directly for icons used

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Box from './components/Box';
 import ReactGA from 'react-ga';
 import 'bulma';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCode, faMusic } from '@fortawesome/free-solid-svg-icons'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCode, faMusic, faGithub } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCode, faMusic)
+// library.add(faCode, faMusic, faGithub)
 
 ReactGA.initialize(process.env.REACT_APP_GA_ID);
 
 const info = require('./info.json');
 const logo = require('./logo.png');
 const image = require('./background.jpg');
+const faGithub = require('./github-brands.svg');
 
 class App extends Component {
     constructor(props) {
@@ -33,7 +34,7 @@ class App extends Component {
                 <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
                     <div className="navbar-brand">
                         <a className="navbar-item" href={window.location.href}>
-                            <img src={logo} alt="NHR" />
+                            <img src={logo} alt={info.fullName} />
                         </a>
                     </div>
 
@@ -53,6 +54,10 @@ class App extends Component {
                             <div className="box">
                                 <h1 className="title is-1">{info.fullName}</h1>
                                 <h4 className="subtitle is-4">{info.position}</h4>
+                                <a className="icon" href={'https://github.com/' + info.github}>
+                                    <img src={faGithub} alt="Github" />{info.github}
+                                </a>
+
                                 <p>{info.bio}</p>
                             </div>
                             {info.boxes.map((box, i) => {
