5a8dd0e17a6005374139e1dd9232c05022c8eab3
Nathanael Raway <nathanael.raway@gmail.com>
2019-03-22 08:49:06 -0500

    Initial commit of pretty functional site

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import Box from './components/Box';
+import ReactGA from 'react-ga';
+import 'bulma';
+import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCode, faMusic } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCode, faMusic)
+
+ReactGA.initialize(process.env.REACT_APP_GA_ID);
+
+const info = require('./info.json');
+const logo = require('./logo.png');
+const image = require('./background.jpg');
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            page: info.tags[0],
+        };
+    }
+
+    componentDidMount = () => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
+                    <div className="navbar-brand">
+                        <a className="navbar-item" href={window.location.href}>
+                            <img src={logo} alt="NHR" />
+                        </a>
+                    </div>
+
+                    <div className="navbar-menu">
+                        <div className="navbar-start">
+                            {/* <a className="navbar-item" href={window.location.href}>{info.fullName}</a> */}
+                            {info.tags.map((tag, i) => {
+                                return <a key={i} className="navbar-item" href="#" onClick={(e) => { e.preventDefault(); this.setState({ page: tag }); }}>{tag}</a>
+                            })}
+                        </div>
+                    </div>
+                </nav>
+
+                <section className="section">
+                    <div className="columns">
+                        <div className="column">
+                            <div className="box">
+                                <h1 className="title is-1">{info.fullName}</h1>
+                                <h4 className="subtitle is-4">{info.position}</h4>
+                                <p>{info.bio}</p>
+                            </div>
+                            {info.boxes.map((box, i) => {
+                                if (box.tag && this.state.page !== box.tag) {
+                                    return '';
+                                }
+                                return (
+                                    <Box
+                                        key={i}
+                                        title={box.title}
+                                        paragraph={box.paragraph}
+                                        items={box.items}
+                                    />
+                                );
+                            })}
+                        </div>
+                        <div className="column">
+                            <img className="rounded" src={image}></img>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class Box extends Component {
+    render() {
+        if (this.props.items) {
+            return (
+                <div className="box">
+                    <h3 className="title is-3">{this.props.title}</h3>
+                    <h6 className="subtitle is-6">{this.props.paragraph}</h6>
+                    {this.props.items.map((item, i) => {
+                        if (typeof item == 'object') {
+                            if (item.where && item.what) {
+                                return (
+                                    <div key={i}>
+                                        <h5 className="title is-5">{item.where}</h5>
+                                        <h6 className="subtitle is-6">{item.what}</h6>
+                                        <p></p>
+                                    </div>
+                                );
+                            }
+                            if (item.title && item.authors) {
+                                return (
+                                    <div key={i}>
+                                        <h5 className="title is-5">{item.title}</h5>
+                                        <h6 className="subtitle is-6">{item.authors}</h6>
+                                        <p></p>
+                                    </div>
+                                );
+                            }
+                        }
+                        return (
+                            <p key={i}>{item}</p>
+                        );
+                    })}
+                </div>
+            );
+        }
+        return '';
+    }
+}
